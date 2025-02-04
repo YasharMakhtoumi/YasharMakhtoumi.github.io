46f18fd60fc821fe4bc017cb0f42796257db3ff0
YasharMakhtoumi <makhtoumi@wisc.edu>
2025-02-04 13:53:43 -0600

    feb4_5

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,21 @@ backToTopButton.addEventListener("click", () => {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.getElementById("nav-links");
+  
+    menuIcon.addEventListener("click", function () {
+      navLinks.classList.toggle("active");
+    });
+  });
+  
+
+
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const images = [
